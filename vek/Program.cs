f371812afec90a19b8a5e4dcf7e102e18f796b8e
sorban.ivan <sorban.ivan@D20-06>
2021-10-26 11:35:40 +0200

    funkci vypocet veku

--- vek/Program.cs
+++ vek/Program.cs
@@ -6,7 +6,23 @@ namespace vek
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"zadej rok narozeni");
+            double roknarozeni;
+            double vek;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("vypocet veku");
+                Console.Write("rok narozeni:");
+                double.TryParse(Console.ReadLine(), out roknarozeni);
+
+                int aktualnirok = DateTime.Now.Year;
+                vek = aktualnirok - roknarozeni;
+                Console.WriteLine($"tvuj vek je {vek}");
+
+
+                Console.ReadLine();
+            }
         }
+        static void Frameboard; 
     }
 }
