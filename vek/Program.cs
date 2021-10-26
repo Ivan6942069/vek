54928212a7222f0bd0b725d8c69773cce160f0c7
sorban.ivan <sorban.ivan@D20-06>
2021-10-26 10:27:28 +0200

    Add project files.

---
+++ vek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"zadej rok narozeni");
+        }
+    }
+}
